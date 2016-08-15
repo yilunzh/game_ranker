9b642d1e63e2a9f8e5354defbad57bcb521f5127
Yilun Zhang <yilunzh@Yiluns-MacBook-Pro.local>
2016-08-14 19:11:35 -0700

    added root route to games

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'games#index'
+
   resources :games, except: [:edit, :update]
   resources :players
   # The priority is based upon order of creation: first created -> highest priority.
