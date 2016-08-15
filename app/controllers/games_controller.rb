6c137d5f3cb4cb60d5540780206670b6648be57d
Yilun Zhang <yilunzh@Yiluns-MacBook-Pro.local>
2016-08-14 22:45:59 -0700

    updated styling

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,7 +4,7 @@ class GamesController < ApplicationController
   # GET /games
   # GET /games.json
   def index
-    @games = Game.all
+    @games = Game.all.order(game_date: :desc)
   end
 
   # GET /games/1
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,5 @@
 
 Player.create(name: "Yilun Zhang", department: "Product", email: "yilun.zhang@carvana.com", wins: 0, losses: 0, rating: 1500)
 Player.create(name: "Suhail Bayot", department: "Engineering", email: "suhail.bayot@carvana.com", wins: 0, losses: 0, rating: 1500)
-Player.create(name: "Chase Barth", department: "Purchasing", email: "chase.barth@carvana.com", wins: 0, losses: 0, rating: 1500)
+Player.create(name: "Chase Barth", department: "Logistics", email: "chase.barth@carvana.com", wins: 0, losses: 0, rating: 1500)
 Player.create(name: "Jason Cohen", department: "Product", email: "jason.cohen@carvana.com", wins: 0, losses: 0, rating: 1500)
